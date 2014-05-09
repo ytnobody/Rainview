1fc70faf339e2bc4cac7a85099b96c7d9e051406
ytnobody <ytnobody@gmail.com>
2014-05-09 18:11:54 +0900

    github--

--- script/install-perl.sh
+++ script/install-perl.sh
@@ -4,8 +4,8 @@ PERLROOT=$HOME/perls/perl-5.18
 PERLVERSION=5.18.2
 
 if [ ! -d $PERLROOT ]; then
-    curl https://raw.github.com/tokuhirom/Perl-Build/master/perl-build | perl - $PERLVERSION $PERLROOT/ 
-    curl -L http://cpanmin.us > $PERLROOT/bin/cpanm && chmod +x $PERLROOT/bin/cpanm
+    curl https://raw.githubusercontent.com/tokuhirom/Perl-Build/master/perl-build | perl - $PERLVERSION $PERLROOT/ 
+    curl https://raw.githubusercontent.com/miyagawa/cpanminus/master/cpanm > $PERLROOT/bin/cpanm && chmod +x $PERLROOT/bin/cpanm
     PERL5LIB=$PERLROOT/lib
     PATH=$PERLROOT/bin:$PATH
     cpanm Carton
